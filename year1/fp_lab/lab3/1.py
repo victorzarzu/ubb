fd2b698901fe21f467a1f07f116d701134b3c7b1
victorzarzu <victorzarzu@gmail.com>
2021-10-06 17:56:09 +0300

    subsequence operations

---
+++ year1/fp_lab/lab3/1.py
@@ -0,0 +1,90 @@
+def read_list():
+  """
+  It reads a list of numbers separated by blanks and returns it
+  params: -
+  return: a list of numbers
+  """
+
+  int_list = [int(x) for x in input().split()]
+  return int_list
+
+
+def operation1(int_list):
+  """
+  Computes and prints the longest ascending subsequences of the argument
+  params: a list of integers
+  return: - 
+  """
+
+  max_len = 0
+  for i in range(0, len(int_list)):
+    j = i + 1
+    while j < len(int_list) and int_list[j] > int_list[j - 1] :
+      j += 1
+    max_len = max(max_len, j - i)
+
+  for i in range(0, len(int_list)):
+    current = [int_list[i]]
+    j = i + 1
+    while j < len(int_list) and int_list[j] > int_list[j - 1]:
+      current.append(int_list[j])
+      j += 1
+    if j - i == max_len:
+      print(current)
+
+
+def operation2(int_list):
+  """
+  Computes and prints the longest subsequences of the argument that has equal elements
+  params: a list of integers
+  return: -
+  """
+  max_len = 0
+  for i in range(0, len(int_list)):
+    j = i + 1
+    while j < len(int_list) and int_list[j] == int_list[j - 1] :
+      j += 1
+    max_len = max(max_len, j - i)
+
+  for i in range(0, len(int_list)):
+    current = [int_list[i]]
+    j = i + 1
+    while j < len(int_list) and int_list[j] == int_list[j - 1]:
+      current.append(int_list[j])
+      j += 1
+    if j - i == max_len:
+      print(current)
+
+
+def functionality():
+  """
+  It manages the operations
+  params: -
+  return: -
+  """
+
+  while True:
+    op = int(input()) 
+    if op == 0: break
+    int_list = read_list()
+    if op == 1:
+      operation1(int_list)
+    elif op == 2:
+      operation2(int_list)
+
+def ui():
+
+  """
+  It displays the UI
+  params: -
+  return: -
+  """
+
+  print("Input: a list of interger numbers separated by blanks")
+  print("Operation 0 => exit") 
+  print("Operation 1 => the longest ascending subsequence in the input list") 
+  print("Operation 2 => the longest subsequence that has equal elements") 
+
+  functionality()
+
+ui()
