6dd854e3f832051dee9bda6e56aab5cecf35cc17
victorzarzu <victorzarzu@gmail.com>
2021-10-09 00:14:00 +0300

    little better

--- year1/sem1/fp_lab/lab3/1.py
+++ year1/sem1/fp_lab/lab3/1.py
@@ -5,9 +5,67 @@ def read_list():
   return: a list of numbers
   """
 
-  int_list = [int(x) for x in input().split()]
+  try:
+    int_list = [int(x) for x in input().split()]
+  except ValueError:
+    print("The numbers are not integers")
+
   return int_list
 
+
+def get_max_len_operation1(int_list):
+  """
+  It computes and returns the length of the longest ascending subsequence
+  params: int_list - a list of integers
+  return: integer
+  """
+
+  max_len = 0
+  for i in range(len(int_list)):
+    j = i + 1
+    while j < len(int_list) and int_list[j] > int_list[j - 1]:
+      j += 1
+    
+    max_len = max(max_len, j - i)
+
+  return max_len
+
+
+def add_list_to_list_of_list(lists, int_list):
+  """
+  It appends a list to a list of lists
+  params: lists - a list of lists of integers; int_list - a list of integers
+  return: -
+  """
+  lists.append(int_list)
+
+
+def get_longest_lists1(max_len, int_list):
+  """
+  It returns a list of the longest ascending lists in int_list
+  params: max_len - integer; int_list - list of integers
+  return: a list of lists of integers
+  """
+
+  lists = []
+  for i in range(0, len(int_list)):
+    j = i + 1
+    while j < len(int_list) and int_list[j] > int_list[j - 1]: j += 1
+    if j - i == max_len:
+      add_list_to_list_of_list(lists, int_list[i:j])           
+
+  return lists
+
+
+def print_list_of_lists(lists):
+  """
+  It prints all lists in the argument
+  params: lists - a list of lists of integers
+  return: -
+  """
+
+  for l in lists:
+    print(l, end = ' ')
 
 def operation1(int_list):
   """
@@ -16,23 +74,43 @@ def operation1(int_list):
   return: - 
   """
 
-  lists = []
+  max_len = get_max_len_operation1(int_list) 
+  lists = get_longest_lists1(max_len, int_list)
+  print_list_of_lists(lists)
+
+
+def get_max_len_operation2(int_list):
+  """
+  It computes and returns the length of the longest constant subsequence
+  params: int_list - a list of integers
+  return: integer
+  """
+
   max_len = 0
-  for i in range(0, len(int_list)):
-    curr = [int_list[i]]
+  for i in range(len(int_list)):
     j = i + 1
-    while j < len(int_list) and int_list[j] > int_list[j - 1]:
-      curr.append(int_list[j])
+    while j < len(int_list) and int_list[j] == int_list[j - 1]:
       j += 1
-
-    max_len = max(max_len, j - i)
     
-    if len(curr) >= max_len:
-      lists.append(curr)
+    max_len = max(max_len, j - i)
 
-  for l in lists:
-    if len(l) == max_len:
-      print(l, end = ' ')
+  return max_len
+
+def get_longest_lists2(max_len, int_list):
+  """
+  It returns a list of the longest constant lists in int_list
+  params: max_len - integer; int_list - list of integers
+  return: a list of lists of integers
+  """
+
+  lists = []
+  for i in range(0, len(int_list)):
+    j = i + 1
+    while j < len(int_list) and int_list[j] == int_list[j - 1]: j += 1
+    if j - i == max_len:
+      add_list_to_list_of_list(lists, int_list[i:j])           
+
+  return lists
 
 def operation2(int_list):
   """
@@ -41,23 +119,38 @@ def operation2(int_list):
   return: -
   """
 
-  lists = []
-  max_len = 0
-  for i in range(0, len(int_list)):
-    curr = [int_list[i]]
-    j = i + 1
-    while j < len(int_list) and int_list[j] == int_list[j - 1]:
-      curr.append(int_list[j])
-      j += 1
+  max_len = get_max_len_operation2(int_list)
+  lists = get_longest_lists2(max_len, int_list)
+  print_list_of_lists(lists)
 
-    max_len = max(max_len, j - i)
+
+def read_and_operation(op):
+    """
+    It reads the list and gives control to the operation function
+    params: op - interger
+    return: -
+    """
+
+
+    if op == 0: exit() 
+    if op != 1 and op != 2:
+      print("Invalid command!")
+      return
+
+    try:
+      int_list = read_list()
+    except:
+      return
+
+    if op == 1:
+      operation1(int_list)
+    elif op == 2:
+      operation2(int_list)
+    else:
+      print("Invalid command")
+    print()
     
-    if len(curr) >= max_len:
-      lists.append(curr)
 
-  for l in lists:
-    if len(l) == max_len:
-      print(l, end = ' ')
 
 def functionality():
   """
@@ -68,13 +161,7 @@ def functionality():
 
   while True:
     op = int(input()) 
-    if op == 0: break
-    int_list = read_list()
-    if op == 1:
-      operation1(int_list)
-    elif op == 2:
-      operation2(int_list)
-    print()
+    read_and_operation(op) 
 
 def ui():
 
