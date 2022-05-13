37ce1594a497a78fff2a5feabb87e6f7f00a56f2
victorzarzu <victorzarzu@gmail.com>
2022-05-13 11:24:05 +0300

    lab7

--- year1/sem2/sda/lab7/IteratorAB.cpp
+++ year1/sem2/sda/lab7/IteratorAB.cpp
@@ -1,10 +1,34 @@
 #include "AB.h"
 #include "IteratorAB.h"
 
+/*
+* Caz favorabil = stiva este goala
+* Complexitate caz favorabil = theta(1)
+*
+* Caz defaborabil = stiva poate contine n elemente
+* Complexitate caz defavorabil = theta(n)
+*
+* Caz mediu = stiva poate contine 0, 1, 2, ..., n elemente
+* Complexitate caz mediu = theta(n)
+*
+* Complexitate totala = O(n)
+*/
 IteratorInordine::IteratorInordine(const AB& _ab) :ab{ _ab }, st(), curent{ NULL } {
 	prim();
 }
 
+/*
+* Caz favorabil = stiva este goala
+* Complexitate caz favorabil = theta(1)
+*
+* Caz defaborabil = stiva poate contine n elemente
+* Complexitate caz defavorabil = theta(n)
+*
+* Caz mediu = stiva poate contine 0, 1, 2, ..., n elemente
+* Complexitate caz mediu = theta(n)
+*
+* Complexitate totala = O(n)
+*/
 void IteratorInordine::prim(){
 	while (!this->st.empty()) {
 		this->st.pop();
@@ -21,10 +45,18 @@ void IteratorInordine::prim(){
 	}
 }
 
+/*
+* Caz favorabil = Caz defavorabil = Caz mediu
+* Complexitate = theta(1)
+*/
 bool IteratorInordine::valid(){
 	return this->curent != NULL;
 }
 
+/*
+* Caz favorabil = Caz defavorabil = Caz mediu
+* Complexitate = theta(1)
+*/
 TElem IteratorInordine::element() {
 	if (!valid()) {
 		throw exception();
@@ -32,6 +64,18 @@ TElem IteratorInordine::element() {
 	return this->curent->e;
 }
 
+/*
+* Caz favorabil = nodul din capul stivei nu are subarbore drept
+* Complexitate caz favorabil = theta(1)
+*
+* Caz defaborabil = subarborele drept al nodului curent poate contine n - 1 noduri
+* Complexitate caz defavorabil = theta(n)
+*
+* Caz mediu = in subarborele drept al nodului curent se pot parcurge 0, 1, ... , n - 1 alte noduri
+* Complexitate caz mediu = theta(n)
+*
+* Complexitate totala = O(n)
+*/
 void IteratorInordine::urmator(){
 	this->curent = this->st.top();
 	this->st.pop();
@@ -53,10 +97,34 @@ void IteratorInordine::urmator(){
 	}
 }
 
+/* 
+* Caz favorabil = stiva este goala
+* Complexitate caz favorabil = theta(1)
+* 
+* Caz defaborabil = stiva poate contine n/2 elemente
+* Complexitate caz defavorabil = theta(n)
+*
+* Caz mediu = stiva poate contine 0, 1, 2, ..., n / 2 elemente
+* Complexitate caz mediu = theta(n)
+*
+* Complexitate totala = O(n)
+*/
 IteratorPreordine::IteratorPreordine(const AB& _ab) :ab{ _ab }, st(), curent{ ab.radacina } {
 	this->prim();
 };
 
+/* 
+* Caz favorabil = stiva este goala
+* Complexitate caz favorabil = theta(1)
+* 
+* Caz defaborabil = stiva poate contine n/2 elemente
+* Complexitate caz defavorabil = theta(n)
+* 
+* Caz mediu = stiva poate contine 0, 1, 2, ..., n / 2 elemente
+* Complexitate caz mediu = theta(n)
+* 
+* Complexitate totala = O(n)
+*/
 void IteratorPreordine::prim(){
 	while (!this->st.empty()) {
 		this->st.pop();
@@ -64,11 +132,18 @@ void IteratorPreordine::prim(){
 	this->curent = this->ab.radacina;
 }
 
-
+/*
+* Caz favorabil = Caz defavorabil = Caz mediu
+* Complexitate = theta(1)
+*/
 bool IteratorPreordine::valid(){
 	return this->curent != NULL;
 }
 
+/*
+* Caz favorabil = Caz defavorabil = Caz mediu
+* Complexitate = theta(1)
+*/
 TElem IteratorPreordine::element(){
 	if (!valid()) {
 		throw exception();
@@ -76,6 +151,18 @@ TElem IteratorPreordine::element(){
 	return this->curent->e;
 }
 
+/*
+* Caz favorabil = nu exista subarbore drept
+* Complexitate favorabil = theta(1)
+*
+* Caz defavorabil = exista subarbore drept
+* Complexitate defavorabil = theta(1)
+* 
+* Caz mediu = poate exista sau nu subarbore drept
+* Complexitate caz mediu = theta(1)
+* 
+* Complexitate = theta(1)
+*/
 void IteratorPreordine::urmator(){
 	if (!valid()) {
 		throw exception();
@@ -97,16 +184,38 @@ void IteratorPreordine::urmator(){
 
 }
 
+/*
+* Caz faborabil = stiva este vida si radacina are doar un nod arborele stang
+* Complexitate caz favorabil = theta(1)
+*
+* Caz defavorabil = stiva este plina si arborele este degenerat (are o structura de lista de valori de exemplu)
+* Complexitate caz defavorabil = 2 * theta(n) = theta(n)
+*
+* Caz mediu = stiva poate sa aiba 0, 1, 2, ..., n elemente, iar while-ul se poate executa de 1, 2, ..., n ori
+* Complexitate caz mediu = 2 * theta(n / 2) = theta(n)
+*
+* Complexitate totala = O(n)
+*/
 IteratorPostordine::IteratorPostordine(const AB& _ab) :ab{ _ab }, curent{ NULL }, st() {
 	prim();
 }
 
+/* 
+* Caz faborabil = stiva este vida si radacina are doar un nod arborele stang
+* Complexitate caz favorabil = theta(1)
+* 
+* Caz defavorabil = stiva este plina si arborele este degenerat (are o structura de lista de valori de exemplu)
+* Complexitate caz defavorabil = 2 * theta(n) = theta(n)
+* 
+* Caz mediu = stiva poate sa aiba 0, 1, 2, ..., n elemente, iar while-ul se poate executa de 1, 2, ..., n ori
+* Complexitate caz mediu = 2 * theta(n / 2) = theta(n)
+* 
+* Complexitate totala = O(n)
+*/
 void IteratorPostordine::prim(){
 	while (!this->st.empty()) {
 		this->st.pop();
 	}
-
-	
 
 	this->curent = ab.radacina;
 	while (this->curent != NULL) {
@@ -134,11 +243,18 @@ void IteratorPostordine::prim(){
 
 }
 
-
+/*
+* Caz favorabil = Caz defavorabil = Caz mediu
+* Complexitate = theta(1)
+*/
 bool IteratorPostordine::valid(){
 	return this->curent != NULL;
 }
 
+/*
+* Caz favorabil = Caz defavorabil = Caz mediu
+* Complexitate = theta(1)
+*/
 TElem IteratorPostordine::element(){
 	if (!valid()) {
 		throw exception();
@@ -146,6 +262,21 @@ TElem IteratorPostordine::element(){
 	return this->curent->e;
 }
 
+/*
+* Caz favorabil - stiva este goala sau in varful stivei avem un element a caruti subarbore drept l-am vizitat
+* Complexitate caz defavorabil = theta(1)
+* 
+* Caz defavorabil - am apelat prim imediat anterior, iar arborele are de exemplu forma 
+    //       1
+    //      / \
+    //     2   3
+    //    / \   
+	//   5   6
+	//      /
+	//     ...
+	//     /
+	//    n
+*/
 void IteratorPostordine::urmator(){
 	if (!valid()) {
 		throw exception();
@@ -176,10 +307,34 @@ void IteratorPostordine::urmator(){
 	}
 }
 
+/*
+* Caz favorabil = coada este goala
+* Complexitate caz favorabil = theta(1)
+*
+* Caz defaborabil = coada poate contine n elemente
+* Complexitate caz defavorabil = theta(n)
+*
+* Caz mediu = coada poate contine 0, 1, 2, ..., n elemente
+* Complexitate caz mediu = theta(n)
+*
+* Complexitate totala = O(n)
+*/
 IteratorLatime::IteratorLatime(const AB& _ab) :ab{ _ab }, curent{ NULL }, q(){
 	prim();
 }
 
+/*
+* Caz favorabil = coada este goala
+* Complexitate caz favorabil = theta(1)
+*
+* Caz defaborabil = coada poate contine n elemente
+* Complexitate caz defavorabil = theta(n)
+*
+* Caz mediu = coada poate contine 0, 1, 2, ..., n elemente
+* Complexitate caz mediu = theta(n)
+*
+* Complexitate totala = O(n)
+*/
 void IteratorLatime::prim(){
 	while (!this->q.empty()) {
 		q.pop();
@@ -187,11 +342,18 @@ void IteratorLatime::prim(){
 	this->curent = this->ab.radacina;
 }
 
-
+/*
+* Caz favorabil = Caz defavorabil = Caz mediu
+* Complexitate = theta(1)
+*/
 bool IteratorLatime::valid(){
 	return this->curent != NULL;
 }
 
+/*
+* Caz favorabil = Caz defavorabil = Caz mediu
+* Complexitate = theta(1)
+*/
 TElem IteratorLatime::element(){
 	if (!valid()) {
 		throw exception();
@@ -199,6 +361,10 @@ TElem IteratorLatime::element(){
 	return this->curent->e;
 }
 
+/*
+* Caz favorabil = Caz defavorabil = Caz mediu
+* Complexitate = theta(1)
+*/
 void IteratorLatime::urmator(){
 	if (!valid()) {
 		throw exception();
