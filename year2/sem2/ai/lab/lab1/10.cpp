164bb934492ed06a78d2875e6fc1dcd053c40435
victorzarzu <victorzarzu@gmail.com>
2023-03-03 15:00:56 +0200

    lab1 ai

--- year2/sem2/ai/lab/lab1/10.cpp
+++ year2/sem2/ai/lab/lab1/10.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
-#include <fstream>
-#include <string>
+#include <vector>
+#include <assert.h>
 
-using std::string;
-using std::ifstream;
-using std::cout;
+using std::vector;
 
 class Solution {
   private:
-    int n, m;
-    bool **matrix;
 
-    int find_first_one(int m, int one_start, bool* row) {
-      int left = 1, right = one_start - 1, mid, response = m + 1;
+    /*
+      Metoda care returneaza indicele primului 1 pana la valoarea one_start din vectorul row
+      @params: m - numarul de elemente din vector
+               one_start - indicele maxim din spatiul de cautare
+               row - vectorul in care se cauta primul 1
+      @return: int - indicele unde se afla primul 1 in intervalul [1, one_start] daca exista un 1 in acest interval si m altfel
+      Complexitate timp: O(logm)
+      Complexitate spatiu: O(1)
+    */
+    static int find_first_one(int m, int one_start, vector<bool>& row) {
+      int left = 0, right = one_start - 1, mid, response = m;
 
       while(left <= right) {
         mid = left + ((right - left) >> 1);
@@ -30,31 +35,21 @@ class Solution {
 
   public:
     
-    Solution(string filename) {
-      ifstream f(filename);
-      
-      f>>n>>m;
-
-      matrix = new bool*[n];
-      for(int i = 0;i < n;++i) {
-        matrix[i] = new bool[m + 1];
-      }
-
-      for(int i = 0;i < n;++i) {
-        for(int j = 1;j <= m;++j) {
-          f>>matrix[i][j];
-        }
-      }
-
-      f.close();
-    }
-
-    int solve() {
+    /*
+      Metoda care returneaza numarul liniei cu cei mai multi 1 dintr-o matrice
+      @params: n - numarul de randuri din matrice
+               m - numarul de coloane din matrice
+               matrix - matricea
+      @return: int - numarul liniei care contine cele mai multe elemente de 1 sau -1 daca matricea nu are randuri
+      Complexitate timp: O(n*logm)
+      Complexitate spatiu: O(1)
+    */
+    static int solve(int n, int m, vector<vector<bool> >& matrix) {
       int response = -1;
-      int one_start = m + 1;
+      int one_start = m;
 
       for(int i = 0;i < n;++i) {
-        if(one_start <= m && !matrix[i][one_start]) {
+        if(one_start < m && !matrix[i][one_start]) {
           continue; 
         }
       
@@ -69,19 +64,39 @@ class Solution {
 
       return response + 1;
     }
-
-    ~Solution() {
-      for(int i = 0;i < n;++i) {
-        delete[] matrix[i];
-      }
-      delete[] matrix;
-    }
 };
 
 int main() {
   
-  Solution sol("10.in");
-  cout<<sol.solve();
+  vector<vector<bool> > unu {{0, 0, 0, 1, 1},
+                              {0, 0, 0, 1, 1},
+                              {0, 0, 0, 0, 1},
+                              {0, 0, 1, 1, 1},
+                              {0, 0, 0, 1, 1}};
+
+  vector<vector<bool> > doi {{0, 1, 1, 1, 1},
+                              {1, 1, 1, 1, 1},
+                              {0, 0, 0, 0, 1},
+                              {0, 0, 1, 1, 1},
+                              {0, 0, 0, 1, 1}};
+
+  vector<vector<bool> > trei {{0, 1, 1, 1},
+                              {0, 0, 1, 1},
+                              {0, 1, 1, 1},
+                              {0, 0, 1, 1},
+                              {0, 0, 0, 1}};
+
+  vector<vector<bool> > patru {{0, 0, 0, 0, 1, 1, 1},
+                              {0, 0, 0, 0, 0, 1, 1},
+                              {0, 0, 0, 0, 0, 1, 1},
+                              {0, 0, 1, 1, 1, 1, 1},
+                              {0, 1, 1, 1, 1, 1, 1},
+                              {0, 0, 0, 1, 1, 1, 1}};
+
+  assert(Solution::solve(5, 5, unu) == 4);
+  assert(Solution::solve(5, 5, doi) == 2);
+  assert(Solution::solve(5, 4, trei) == 1);
+  assert(Solution::solve(6, 7, patru) == 5);
 
   return 0;
 }
